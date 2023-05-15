14ae7bfd788928deecbc474a59ba76c92e40f9c2
Chrudoš Vorlíček <chrudos.vorlicek@gmail.com>
2023-05-15 06:54:43 +0200

    Create Dockerfile and build configuration

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM php:8.2-cli-bullseye
+
+COPY --from=mlocati/php-extension-installer /usr/bin/install-php-extensions /usr/local/bin/
+COPY --from=liteacz/whalesome:v0.0.1-alpha.1 /opt/whalesome /opt/whalesome
+
+COPY ./shared.sh /usr/local/bin/shared
+
+RUN echo "Installing shared dependencies" \
+    && mv /opt/whalesome/bin/* /usr/local/bin \
+    && chmod +x /usr/local/bin/shared \
+    && /usr/local/bin/shared
+
+ENV LANG cs_CZ.UTF-8
+ENV LC_ALL cs_CZ.UTF-8
+
+WORKDIR /var/www
+
+ENTRYPOINT ["docker-entrypoint"]
---
+++ shared.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+echo "Installing shared dependencies" \
+  && echo "Installing laravel preset" \
+  && apt-get update \
+  && apt-get install -y libtool wget libpng-dev zlib1g-dev libzip-dev \
+                        libfreetype6-dev libjpeg62-turbo-dev libgd-dev \
+                        libonig-dev libpq-dev libicu-dev ghostscript \
+                        python3-pip htop less iproute2 libboost-all-dev sqlite3 locales chromium \
+  && localedef -i cs_CZ -c -f UTF-8 -A /usr/share/locale/locale.alias cs_CZ.UTF-8 \
+  && apt-get install -y $PHPIZE_DEPS\
+  && lit preset laravel \
+  && lit install php/composer \
+  && echo "Installing swoole extension (required by Octane)" \
+  && pecl install swoole \
+  && docker-php-ext-enable swoole\
+  && echo "Installing php extensions" \
+  && docker-php-ext-install iconv pcntl zip gd mbstring intl pdo_pgsql sockets\
+  && echo "Clean apt cache" \
+  && apt-get clean \
+  && apt-get autoclean \
+  && rm -rf /var/cache/apt/* \
+  && wget -O "/usr/local/bin/local-php-security-checker" "https://github.com/fabpot/local-php-security-checker/releases/download/v2.0.4/local-php-security-checker_2.0.4_linux_amd64" \
+  && chmod a+x /usr/local/bin/local-php-security-checker \
+  && wget -O "/usr/local/bin/phpdoc" "https://phpdoc.org/phpDocumentor.phar" \
+  && chmod a+x /usr/local/bin/phpdoc \
+  && pip3 install supervisor
+
